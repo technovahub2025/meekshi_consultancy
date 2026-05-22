03502a411c1ca925fb2751892888573f8a956255
technovahub@2025 <technovahubcareer@gmail.com>
2026-05-22 16:23:59 +0530

    remove basename from BrowserRouter and base path from Vite config

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/test_meekshiConsultancy">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
