8702b53375c426659170d7f3d84d9bdccbf98367
technovahub@2025 <technovahubcareer@gmail.com>
2026-06-03 11:57:39 +0530

    update site title and favicon

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './theme.css'
 import App from './App.jsx'
+import siteIcon from './assets/meenakshi consultancy.jpeg'
+
+document.title = 'meekshi'
+
+const existingIcon = document.querySelector('link[rel="icon"]')
+if (existingIcon) {
+  existingIcon.href = siteIcon
+  existingIcon.type = 'image/jpeg'
+} else {
+  const link = document.createElement('link')
+  link.rel = 'icon'
+  link.type = 'image/jpeg'
+  link.href = siteIcon
+  document.head.appendChild(link)
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
